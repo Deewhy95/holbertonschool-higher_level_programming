abc323576f15de3255b708bd25a7087c5f9cb4d0
arcewp15 <970@holbertonschool.com>
2019-12-17 16:48:08 -0500

    Modify and include conditions to print the element, This Python script have a function that replace an element in a list at specific position without modifying the original list (like in C)

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    cp = my_list[:]
-    if idx >= 0 and idx < len(my_list):
+    if my_list is None:
+        pass
+    elif idx >= 0 and idx < len(my_list):
+        cp = my_list[:]
         cp[idx] = element
         return cp
