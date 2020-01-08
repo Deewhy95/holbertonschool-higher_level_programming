01a3afa1eed10237cb3c3cf92d4bea8d196860fa
arcewp15 <970@holbertonschool.com>
2020-01-08 14:47:27 -0500

    Modify the way to execute the code, Python script with a function that printd x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for index in my_list[:x]:
-        try:
-            print('{}'.format(index), end='')
-        except IndexError:
-            continue
-            return index
-    print()
-    return index
+    try:
+        for index in range(x):
+            print('{}'.format(my_list[index]), end='')
+        print()
+        return x
+    except IndexError:
+        print()
+        return index
