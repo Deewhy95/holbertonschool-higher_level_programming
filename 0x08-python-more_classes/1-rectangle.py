fcfb4d558772345fc73eac24ad59e403c7eba8de
arcewp15 <970@holbertonschool.com>
2020-01-16 01:02:05 -0500

    This Python script defines a rectangle with private instance attribute (height and width)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 class Rectangle:
+""" define class Rectangle with some attributes: width and height """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
