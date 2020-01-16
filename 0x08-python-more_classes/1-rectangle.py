22c65457b7e09166002592d7638249d6190e9c1b
arcewp15 <970@holbertonschool.com>
2020-01-16 14:49:28 -0500

    Change the sense of the code, organize the conditions, This Python script defines a rectangle with private instance attribute (height and width)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 class Rectangle:
-    """ define class Rectangle with some attributes: width and height """
+    """
+    define class Rectangle with some attributes: width and height
+    """
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if type(value) is not int:
-            raise TypeError('height must be an integer')
-        elif value < 0:
-            raise ValueError('height must be >=0')
-        else:
-            self.__height = value
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -27,6 +16,19 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError('width must be an integer')
         elif value < 0:
-            raise ValueError('width must be >= 0')
+            raise ValueError('width must be >=0')
         else:
             self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
