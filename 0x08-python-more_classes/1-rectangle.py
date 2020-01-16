d6e54f4e65fc149bdc6cbbef486de8af14b2b619
arcewp15 <970@holbertonschool.com>
2020-01-16 01:14:05 -0500

    This Python script defines a rectangle with private instance attribute (height and width)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle:
     def width(self, value):
         if type(value) is not int:
             raise TypeError('width must be an integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError('width must be >=0')
         else:
             self.__width = value
@@ -26,7 +26,7 @@ class Rectangle:
     def height(self, value):
         if type(value) is not int:
             raise TypeError('height must be an integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError('height must be >= 0')
         else:
             self.__height = value
