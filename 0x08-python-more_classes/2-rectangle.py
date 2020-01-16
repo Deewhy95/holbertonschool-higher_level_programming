e4e32101f682f4a0916bf27bf6f2aaf163806760
arcewp15 <970@holbertonschool.com>
2020-01-16 16:28:14 -0500

    This Python script defines a rectangle with private instance attribute (height and width) and also area and perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -42,4 +42,5 @@ class Rectangle:
     def perimeter(self):
         if self.width == 0 and self.height == 0:
             return 0
-        return (2*(self.width + self.height))
+        else:
+            return (2*(self.width + self.height))
