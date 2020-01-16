a5c5ffdaa6340620ff29686c0722d4aa8c12f947
arcewp15 <970@holbertonschool.com>
2020-01-16 01:20:53 -0500

    Change the sense of the code, organize the conditions, This Python script defines a rectangle with private instance attribute (height and width)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,19 +6,6 @@ class Rectangle:
         self.height = height
 
     @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if type(value) is not int:
-            raise TypeError('width must be an integer')
-        elif value < 0:
-            raise ValueError('width must be >=0')
-        else:
-            self.__width = value
-
-    @property
     def height(self):
         return self.__height
 
@@ -27,6 +14,19 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError('height must be an integer')
         elif value < 0:
-            raise ValueError('height must be >= 0')
+            raise ValueError('height must be >=0')
         else:
             self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
