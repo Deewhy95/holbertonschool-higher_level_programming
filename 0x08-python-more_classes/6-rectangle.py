cbdc918a354ab87a94698b0222b8c08b91f8d0d9
arcewp15 <970@holbertonschool.com>
2020-01-16 22:07:42 -0500

    Fix the increment during each new instance instantiation, This Python script defines a rectangle with private instance attributes, verify how many instances are in the code

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -11,8 +11,10 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
         self.width = width
         self.height = height
+
 
     @property
     def width(self):
