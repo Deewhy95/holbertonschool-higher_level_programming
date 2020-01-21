7410a3e8878ed498f139abecc3ffc5d4a0f88abc
arcewp15 <970@holbertonschool.com>
2020-01-21 18:51:54 -0500

    Fix open(), This Python script has the function that reads a text file (UTF8) and prints it to stdout, with statement

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,7 +6,7 @@ def read_lines(filename="", nb_lines=0):
     Read the entire file if nb_lines is lower or equal to 0
     OR greater or equal to the total number of lines of the file
     """
-    with open("my_file_0.txt", "r", encoding="utf-8") as f:
+    with open(filename) as f:
         text = f.readlines()
         if nb_lines <= 0 or nb_lines >= len(text):
             for i in text:
