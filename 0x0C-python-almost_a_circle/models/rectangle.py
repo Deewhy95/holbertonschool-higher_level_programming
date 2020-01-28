a8fe738dc5815120b78efbe05ac1ac950781bd70
arcewp15 <970@holbertonschool.com>
2020-01-27 23:31:36 -0500

    Adding AGAIN the file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""" class Rectangle """
+""" Class Rectangle """
 
 from models.base import Base
 
 
 class Rectangle(Base):
     """
-    Creates the class Rectangle that inherits from Base
+    Creates a class Rectangle that inherits from Base
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ class constructor """
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
