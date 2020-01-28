044c8dae4fa280ab5aac33f1a6c6e46170c9367f
arcewp15 <970@holbertonschool.com>
2020-01-28 18:49:35 -0500

    Adding the public method def update(self, *args, **kwargs) that assigns attributes to the Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,3 +26,25 @@ class Square(Rectangle):
         """ setter, validations """
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """ Public method that assigns a key/value argument to attributes """
+        if len(args) is not 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 4:
+                self.x = args[2]
+            if len(args) >= 5:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.width = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
