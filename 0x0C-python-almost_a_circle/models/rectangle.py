da8d4b5dc7b654e2126cb8ff62b5e0a3f1b41379
arcewp15 <970@holbertonschool.com>
2020-01-27 23:11:52 -0500

    Fix pep8, import Base for inheritance

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """ class Rectangle that inherits from Base """
+from models.base import Base
 
 
 class Rectangle(Base):
     """
     Creates the class Rectangle that inherits from Base
     """
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """ class constructor """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
