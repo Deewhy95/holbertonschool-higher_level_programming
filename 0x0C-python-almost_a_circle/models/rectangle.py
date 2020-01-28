d39a05145d87109c8603cd4b3a3dfd12c698779e
arcewp15 <970@holbertonschool.com>
2020-01-27 23:20:02 -0500

    Fix pep8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ class Rectangle """
+
 from models.base import Base
 
 
@@ -10,8 +11,8 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ class constructor """
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
