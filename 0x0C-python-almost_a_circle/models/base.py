9d983a6452feebf3d87f75f9334d222ac0b8aa47
arcewp15 <970@holbertonschool.com>
2020-01-28 20:00:04 -0500

    Fix conditions, adding the static method def to_json_string(list_dictionaries): that returns the JSON string representation of list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """ JSON string representation """
-        if len(list_dictionaries) is 0 or list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) is 0:
             return "[]"
         else:
             return(json.dumps(list_dictionaries))
