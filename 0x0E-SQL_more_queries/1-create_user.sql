b917b25f8a180c1e2bf3fb247f42cfa9d81ba4a5
marcewp15 <970@holbertonschool.com>
2020-03-03 19:35:44 +0000

    Fix comments and code, MYSQL script that creates the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
--- MYSQL script that creates the MySQL server user user_0d_1.
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- MYSQL script that creates the MySQL server user user_0d_1: should have all
+-- privileges on MySQL server, password should be set to user_0d_1_pwd.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
