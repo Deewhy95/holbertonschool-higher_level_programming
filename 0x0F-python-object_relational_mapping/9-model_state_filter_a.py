b0ebada4f2a3b1ad79e5c8329e504d102727d085
marcewp15 <970@holbertonschool.com>
2020-03-30 02:01:21 +0000

    This script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Prints the first State object from the database hbtn_0e_6_usa
+lists all State objects that contain the letter a from the database hbtn_0e_6_usa
 """
 if __name__ == '__main__':
     from model_state import Base, State
