f9b0492fdff596089a34f50bcc8962132ccbfec5
marcewp15 <970@holbertonschool.com>
2020-04-17 01:42:52 +0000

    This Python script fetches a Holberton's intranet URL

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Fetching URLs with urllib """
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+   html = response.read()
+   print("Body response:")
+   print("\t- type: {}\n\t-content: {}\n\t- utf8 content: {}"
+         .format(type(html), html, html.decode('utf-8')))
