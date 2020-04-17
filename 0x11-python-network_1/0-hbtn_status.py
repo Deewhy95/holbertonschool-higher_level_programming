49bbd1c4a3a42743ef52f6d1f35c78cf1c243614
marcewp15 <970@holbertonschool.com>
2020-04-17 01:55:13 +0000

    Fix requeriments, This Python script fetches a Holberton's intranet URL

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
-        print("\t- type: {}\n\t-content: {}\n\t- utf8 content: {}"
+        print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
               .format(type(html), html, html.decode('utf-8')))
