fbe0473bbf458e16a0374ef78387127d2a60bb37
marcewp15 <970@holbertonschool.com>
2020-05-09 00:15:13 +0000

    This Python script takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This Python script takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) != 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    req = requests.post(url, data={'q': q}).json()
+    try:
+        if req == 0:
+            print('No result')
+        else:
+            print('[{}] {}'.format(response.get('id'), response.get('name')))
+    except:
+        print('Not a valid JSON')
