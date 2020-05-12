919e344ae0af133df28a45b322b2c7b42198a812
marcewp15 <970@holbertonschool.com>
2020-05-12 04:22:55 +0000

    This script returns the reversed version of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/* returns the number of occurrences in a list */
+/* returns the reversed version of a list */
 exports.esrever = function (list) {
   const rev = [];
   for (const i in list) {
